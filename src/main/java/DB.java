79c589e2038b06a4bc90be67cf3b4c416751cf52
BillyAyiera <ayierabilly1@gmail.com>
2020-05-11 23:10:26 +0300

    pom.xml file modification

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -2,4 +2,7 @@ import org.sql2o.*;
 
 public class DB {
     public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/organisational_api", "billyayiera", "theboyme");
+//    public static String connectionString = "jdbc:postgresql://ec2-184-72-235-80.compute-1.amazonaws.com:5432/db9mi904g6otb2";
+//    public static Sql2o sql2o = new Sql2o(connectionString, "ocaoyylacmruev", "e21560efd2757a2eb13da85a421e410791d3ac68e83f5d548f13440ac5bc46f1");
+
 }

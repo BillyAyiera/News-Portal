847f7503d97adb88b20dc62750aa2ebbb8d65224
BillyAyiera <ayierabilly1@gmail.com>
2020-05-12 00:15:37 +0300

    heroku deployment

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,8 +1,8 @@
 import org.sql2o.*;
 
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/organisational_api", "billyayiera", "theboyme");
-//    public static String connectionString = "jdbc:postgresql://ec2-184-72-235-80.compute-1.amazonaws.com:5432/db9mi904g6otb2";
-//    public static Sql2o sql2o = new Sql2o(connectionString, "ocaoyylacmruev", "e21560efd2757a2eb13da85a421e410791d3ac68e83f5d548f13440ac5bc46f1");
+//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/organisational_api", "billyayiera", "theboyme");
+    public static String connectionString = "jdbc:postgresql://ec2-34-195-169-25.compute-1.amazonaws.com:5432/d5rs9jb23pkh5f";
+    public static Sql2o sql2o = new Sql2o(connectionString, "rjurasklwumipz", "db442726015b11251659b7a3b6fddaa89463d2dbd84aafad1421693ef3b334a9");
 
 }

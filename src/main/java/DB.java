1e9231faa2e7078a4a2afd3ec453d72c0c7f46d9
BillyAyiera <ayierabilly1@gmail.com>
2020-05-12 09:04:11 +0300

    gitignored files

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -2,7 +2,7 @@ import org.sql2o.*;
 
 public class DB {
 //    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/organisational_api", "billyayiera", "theboyme");
-    public static String connectionString = "jdbc:postgresql://ec2-34-195-169-25.compute-1.amazonaws.com:5432/d5rs9jb23pkh5f";
-    public static Sql2o sql2o = new Sql2o(connectionString, "rjurasklwumipz", "db442726015b11251659b7a3b6fddaa89463d2dbd84aafad1421693ef3b334a9");
+      public static String connectionString = "jdbc:postgresql://ec2-52-44-166-58.compute-1.amazonaws.com:5432/darf6sp6jvjv2t";
+      public static Sql2o sql2o = new Sql2o(connectionString, "rqtlzrsvcyhwda", "5bcd875a19df92217d320e214bec1d1b3b32e719a4af60cf81cbfc7289f6f300");
 
 }
